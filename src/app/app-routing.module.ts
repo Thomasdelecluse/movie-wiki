203caeef5f31576c933de59ef5c45a50e865d429
Thomasdelecluse <81568452+Thomasdelecluse@users.noreply.github.com>
2023-01-08 22:44:08 +0100

    feat: create root details

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import {RouterModule, Routes} from "@angular/router";
 import {LoginComponent} from "./login/login.component";
 import {HomeComponent} from "./home/home.component";
 import {SearchComponent} from "./search/search.component";
+import {DetailsComponent} from "./details/details.component";
 
 const routes: Routes = [
   { path: '', component: HomeComponent, },
   { path: 'login', component: LoginComponent, },
   { path: 'search', component: SearchComponent, },
+  { path: 'details/:id', component: DetailsComponent, },
 ];
 
 @NgModule({
