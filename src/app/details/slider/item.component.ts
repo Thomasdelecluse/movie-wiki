ced988925dd8b003dca6e966dcd3900212c1cbde
ThomasDelecluseRedlab <81568452+Thomasdelecluse@users.noreply.github.com>
2023-01-18 17:24:46 +0100

    feat: add actor in slider component and add css to the slider

--- src/app/details/slider/item.component.ts
+++ src/app/details/slider/item.component.ts
@@ -1,12 +1,20 @@
 import {Component, Input} from "@angular/core";
 import {Cast} from "../details.component";
+import {BASE_URL} from "../../constant/components.constant";
 
 @Component({
   selector: 'app-detail-slider-item',
   templateUrl: './item.component.html',
+  styleUrls: ['./item.component.css']
 })
 export class ItemComponent {
-
+  baseUrl = BASE_URL;
   @Input() item: Cast | undefined;
+
+
+  formatName(name: string | undefined): string {
+    if (typeof name === 'undefined') return '';
+    return name.replace('(archive footage) (uncredited)', '');
+  }
 
 }
