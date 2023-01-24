a5b90f558528f03d053a964bc6555605c8b207e6
Thomasdelecluse <81568452+Thomasdelecluse@users.noreply.github.com>
2023-01-24 18:06:02 +0100

    feat: upcoming request api

--- src/app/upcoming/upcoming.component.ts
+++ src/app/upcoming/upcoming.component.ts
@@ -1,8 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import {TmdbService} from "../../service/TmdbService";
+import {BASE_URL} from "../constant/components.constant";
+import {PopularMovie} from "../../service/popularMovie.service";
+import {Router} from "@angular/router";
 export interface Result {
   popularity: number;
   release_date: string;
+  poster_path: string;
+  title: string;
 }
 
 @Component({
@@ -12,9 +17,10 @@ export interface Result {
 })
 export class UpcomingComponent implements OnInit {
   upcomingMovie: Result[] | undefined;
-  bestUpcomingMovie: any[] | undefined;
+  bestUpcomingMovie: Result[] | undefined;
+  base_url = BASE_URL;
 
-  constructor(private TmdbService: TmdbService) { }
+  constructor(private TmdbService: TmdbService, private popularMovie: PopularMovie, private router: Router) { }
 
   ngOnInit(): void {
     this.TmdbService.getAllUpcomingMovies().then(r => {
@@ -23,8 +29,18 @@ export class UpcomingComponent implements OnInit {
       console.log('cjnhfu', r.filter(movie => movie.original_title.includes('Ant-Man')));
 
       this.bestUpcomingMovie = r.filter(movie => new Date(movie.release_date).getTime() > new Date().getTime() && movie.popularity > 20);
+      this.bestUpcomingMovie = this.bestUpcomingMovie.slice(0, 8);
       console.log("bm",this.bestUpcomingMovie);
     });
+  }
+  getTypeAndId(movie: any) {
+    this.router.navigate(['/details', movie.id,"movie"])
+      .then(() => {
+        console.log('Navigation successful!');
+      })
+      .catch(error => {
+        console.log('Navigation failed: ' + error);
+      });
   }
 
 }
